fdee588eb2e5ac51a304c2625675547622b18395
k-and-k-dev <k.k.llc.2021@gmail.com>
2023-07-05 19:59:07 +0900

    タイトルを画像化

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,23 @@
 "use client";
 // react
 import React from "react";
+// next.js
+import Image from "next/image";
 // style
-// import "../styles/globals.scss";
 import styles from "./page.module.scss";
+// resource
+import TitleImagePath from "../images/Title.png";
+// components
 import { Spacer } from "@/components/atoms/Spacer";
 import { Form } from "@/components/organisms/Form";
 import { ContactForm } from "@/components/organisms/ContactForm";
-// components
 
 export default function Home() {
     return (
         <>
             <Spacer size="10px" />
             <div className={styles.main}>
-                <h1 className={styles.title}>
-                    【TFT(チームファイト タクティクス)】
-                    <br />
-                    かわいい自己紹介カード作成ツール
-                </h1>
-                <p className={styles.description}>
-                    TFT(チームファイト タクティクス)のかわいい自己紹介カードが
-                    <br className={styles.br} />
-                    簡単に作成・編集できるツールです！🥳🎉
-                    <br />
-                    プレビューを見ながら文字入れをして
-                    <br className={styles.br} />
-                    あなただけの自己紹介カードをつくろう！
-                </p>
+                <Image className={styles.title_img} src={TitleImagePath} width={1500} height={309} alt="Title" />
                 <Form />
                 <ContactForm />
             </div>
