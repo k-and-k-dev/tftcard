27c79e3adaf9e6351b205644845b5bebfa076556
k-and-k-dev <k.k.llc.2021@gmail.com>
2023-07-11 15:32:41 +0900

    メタデータのdescriptionを正式なものに変更。

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,8 @@ import { Header } from "@/components/Header";
 
 export const metadata = {
     title: "TFT Profile Card",
-    description: "",
+    description:
+        "リーグオブレジェンド（League of Legends、LoL）で有名なライアットゲームズ（Riot Games）が提供しているゲーム、TFT（チームファイトタクティクス）のプロフィールカードが簡単に作成・編集出来るサイトです。あなただけの自己紹介カードを作ってTwitter等で投稿してね。",
     themeColor: "#000000",
     formatDetection: {
         telephone: false,
