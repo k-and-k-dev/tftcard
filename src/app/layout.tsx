0f376aedeb7e3de34b995e7ef48897e3fb79de56
k-and-k-dev <k.k.llc.2021@gmail.com>
2023-07-28 14:47:41 +0900

    metadata に og と twitter 設定を追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,14 @@ import "./layout.scss";
 // components
 import { Header } from "@/components/Header";
 
+const siteName = "TFT Profile Card 自己紹介カード作成ツール";
+const description =
+    "TFT（チームファイトタクティクス）のプロフィールカードが簡単に作成・編集出来るサイトです。かわいいタクティシャンを選んであなただけの自己紹介カードを作ってTwitter等で投稿してね。";
+const url = "https://tftcard.vercel.app";
+
 export const metadata = {
-    title: "TFT Profile Card 自己紹介カード作成ツール",
-    description:
-        "TFT（チームファイトタクティクス）のプロフィールカードが簡単に作成・編集出来るサイトです。かわいいタクティシャンを選んであなただけの自己紹介カードを作ってTwitter等で投稿してね。",
+    title: siteName,
+    description: description,
     themeColor: "#000000",
     formatDetection: {
         telephone: false,
@@ -15,6 +19,21 @@ export const metadata = {
         icon: "/favicon.ico",
     },
     viewport: "width=device-width,initial-scale=1.0,maximum-scale=1.0",
+    openGraph: {
+        title: siteName,
+        description,
+        url,
+        siteName,
+        locale: "ja_JP",
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: siteName,
+        description,
+        site: "@tftprofilecard",
+        creator: "@tftprofilecard",
+    },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
